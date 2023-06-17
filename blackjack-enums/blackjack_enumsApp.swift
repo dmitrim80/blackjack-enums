b16266d6919d60987d2a8ae47150785eaa74205a
Dmitri Morozov <114444260+dmitrim80@users.noreply.github.com>
2023-06-17 11:45:08 -0400

    Initial Commit

---
+++ blackjack-enums/blackjack_enumsApp.swift
@@ -0,0 +1,17 @@
+//
+//  blackjack_enumsApp.swift
+//  blackjack-enums
+//
+//  Created by Dmitri Morozov on 6/17/23.
+//
+
+import SwiftUI
+
+@main
+struct blackjack_enumsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
